462c95695b32f32dee123845dd589dc7e9852378
kambleps <125243027+kambleps@users.noreply.github.com>
2023-03-18 15:09:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calculateMinCost() {
   //your code here
   var inputdata =document.querySelector("#rope-lengths").value;
 	var inputArr = inputdata.split(",");
-	for(var i=0;i<inputArr.length;i++)
+	for(var i=0; i<inputArr.length; i++)
 		{
 			inputArr[i] = Number(inputArr[i]);
 		}
